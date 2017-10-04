0365c74cd6e279c1c2a957973140a6dcf94ef138
Wellison <wellison28@gmail.com>
2017-10-04 08:20:12 -0300

    Alteração action create trucker

--- app/controllers/api/truckers_controller.rb
+++ app/controllers/api/truckers_controller.rb
@@ -2,66 +2,28 @@
 module Api
 class TruckersController < ApplicationController
 	
-def create
-		origin = shipment_params[:origin]
-		destination = shipment_params[:destination]
-		contact = shipment_params[:contact]
-		vehicles = shipment_params[:vehicles]
-		puts :zuaaaaaaaaado
+  def create   
+  	vehicle = trucker_params[:vehicle]
 
-
-		@origin = Origin.new(origin)
-		if @origin.save
-			@destination = Destination.new(destination)
-			if @destination.save
-				@contact = Contact.new(contact)
-				if @contact.save
-					@shipment = Shipment.new(value: shipment_params[:value],
-											 weight_kg: shipment_params[:weight_kg],
-											 contact: @contact,
-											 origin: @origin,
-											 destination: @destination)
-					if @shipment.save
-						for i in 0..(vehicles.count - 1)
-							@vehicle = ShipmentVehicle.new(shipment: @shipment,
-												   vtype: vehicles[i][:type],
-												   body_type: vehicles[i][:body_type])
-							if !@vehicle.save
-								@shipment.destroy
-								@contact.destroy
-								@destination.destroy
-								@origin.destroy	
-								erro = true								
-								break
-							end
-						end	
-						if !erro
-							render json: @shipment, status: :created#, location: @shipment
-						else
-							render json:@vehicle.errors, status: :unprocessable_entity	
-						end	
-					else
-						@contact.destroy
-						@destination.destroy
-						@origin.destroy
-						render json:@shipment.errors, status: :unprocessable_entity	
-					end
-				else
-					@destination.destroy
-					@origin.destroy
-					render json:@contact.errors, status: :unprocessable_entity
-				end	
-			else
-				@origin.destroy
-				render json:@destination.errors, status: :unprocessable_entity
-			end
+    @vehicle = Vehicle.new(vtype: vehicle[:type],
+    					   body_type: vehicle[:body_type])
+    if @vehicle.save
+		@trucker = Trucker.new(name: trucker_params[:name], 
+							   phone: trucker_params[:phone],
+							   vehicle: @vehicle)
+		if @trucker.save
+			render json: @trucker, status: :created, location: [:api, @trucker]
 		else
-			render json:@origin.errors, status: :unprocessable_entity
-		end
-	end
+			@vehicle.destroy
+			render json: @trucker.errors, status: :unprocessable_entity
+		end	      
+    else
+      render json: @vehicle.errors, status: :unprocessable_entity
+    end
+  end
 
-  	def shipment_params
-  		params.require(:shipment).permit!
+  	def trucker_params
+  		params.require(:trucker).permit!
 	end
 
 	def last_location
