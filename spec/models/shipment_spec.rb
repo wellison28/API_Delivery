8aa06a2a1b6d8e76e5e31ead274688e707066b53
Wellison <wellison28@gmail.com>
2017-10-05 00:34:33 -0300

    Testes models shipment e truckers

---
+++ spec/models/shipment_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Shipment, type: :model do
+    context "Validade de um Shipment" do
+    it "Shipment is valid?" do
+      shipment = Shipment.new
+      shipment.value = 1000.0
+      shipment.weight_kg = 500
+      expect(shipment.valid?).to be_falsey
+    end
+
+    it "Shipment is valid?" do    
+      contact = Contact.new(name: "Rodolfo", phone: "(11)7628-8278")
+      origin = Origin.new(city:"São Paulo", state:"SP", load_at:"2017-09-29")
+      destination = Destination.new(city:"São Paulo", state:"SP", deliver_at:"2017-09-29")
+      shipment = Shipment.new(value: 1000.0,
+      						  weight_kg: 500,
+      						  contact: contact,
+      						  destination: destination,
+      						  origin: origin)
+
+      expect(shipment.valid?).to be_truthy
+    end
+  end
+end
---
+++ spec/models/trucker_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Trucker, type: :model do
+   context "Validade de um Trucker" do
+    it "Trucker valid?" do
+      trucker = Trucker.new
+      trucker.name = "Wellison"
+      trucker.phone = "(11)97722-7508"
+      expect(trucker.valid?).to be_falsey
+    end
+
+    it "Trucker valid?" do
+      trucker = Trucker.new
+      trucker.name = "Wellison"
+      trucker.phone = "(11)97722-7508"
+      
+      vehicle = Vehicle.new
+      vehicle.vtype = "Truck"
+      vehicle.body_type = "Sider"
+
+      trucker.vehicle = vehicle
+
+      expect(trucker.valid?).to be_truthy
+    end
+  end
+end
